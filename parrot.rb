0b12646ae81ffe6df41346e0428e115590d4409d
nanaama527 <72888369+nanaama527@users.noreply.github.com>
2020-10-22 12:03:39 -0400

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,15 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(noise)
+    puts ("Squawk!")    
+end
+
+parrot("Squawk!")
+
+def parrot(noise="Squawk!")
+    puts noise
+    return noise
+end
+
+parrot("Squawk!")
